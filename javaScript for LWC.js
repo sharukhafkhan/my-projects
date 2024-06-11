4f2d3223d70ac6c673c9d15321e84235408b6f7b
sharu <sharukhkhanafkhan@gmail.com>
2024-06-11 23:09:12 +0530

    javascript file

---
+++ javaScript for LWC.js
@@ -0,0 +1,21 @@
+// Java script learning path
+/**
+ * variables
+ * Data types
+ * Null vs undefined
+ * Arrow function
+ * spread operator
+ * Destructing
+ * String interpolation
+ * String methods
+ * object methods
+ * Array methods
+ * Promises
+ * Modules import and export
+ * events
+ * setTimeoout vs setInterval
+ * Query selector
+ *
+ */
+
+// variables
